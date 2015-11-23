f3a250173909a8313f96915c1caee3f36bed56bf
Francesco Montefoschi <francesco.monte@gmail.com>
2015-11-23 17:03:46 +0100

    ENGR00315894-57 iMX6SX: Add M4 boot support at soc level
    Implement the override function "arch_auxiliary_core_up" to boot
    Cortex-M4 by executing command "bootaux".
    The parameter "boot_private_data" points to fields where stores
    the stack address and PC address for M4 to run.
    
    Signed-off-by: Ye.Li <B37916@freescale.com>

--- arch/arm/include/asm/imx-common/sys_proto.h
+++ arch/arm/include/asm/imx-common/sys_proto.h
@@ -39,6 +39,10 @@ void init_aips(void);
 void init_src(void);
 void imx_set_wdog_powerdown(bool enable);
 
+#ifdef CONFIG_MX6SX
+int arch_auxiliary_core_up(u32 core_id, u32 boot_private_data);
+#endif
+
 /*
  * Initializes on-chip ethernet controllers.
  * to override, implement board_eth_init()
